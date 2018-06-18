118599520d3c393aee227e3b3f09db8ed6f0610e
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2018-06-18 16:48:48 +0100

    Test 'should give 1 when given 1 as argument' -FAILING

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -2,4 +2,8 @@ import fizzbuzz from './fizzbuzz';
 
 test('does nothing with no argument', () => {
   expect(fizzbuzz()).toBeUndefined();
-});
+})
+
+test('should give 1 when given 1 as argument', () => {
+  expect(fizzbuzz(1)).toEqual([1])
+})
