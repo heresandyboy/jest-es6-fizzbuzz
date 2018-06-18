4c0c94e9ae52e330c41e6d92150c901153d918cf
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2018-06-18 17:23:29 +0100

    TEST - gives an array of [1, 2, "Fizz"] with 3 as an argument - RED

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -11,3 +11,7 @@ test('should give 1 when given 1 as argument', () => {
 test("gives an array of [1, 2] with 2 as an argument", function () {
   expect(fizzbuzz(2)).toEqual([1, 2]);
 });
+
+test("gives an array of [1, 2, \"Fizz\"] with 3 as an argument", function () {
+  expect(fizzbuzz(3)).toEqual([1, 2, "Fizz"]);
+});
