ee6cb0d205c05ff4640d3d1b2fa366ef8ff46a7f
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2018-06-18 17:25:44 +0100

    TEST - gives an array of [1, 2, "Fizz"] with 3 as an argument - GREEN

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,12 +1,16 @@
-export default function fizzbuzz(number) {
-    if (!number)
+export default function fizzbuzz(n) {
+    if (!n)
         return
 
-        let arr = []
-        arr.push(1)
-        if (number === 1) {
-            return arr
-        }
-        arr.push(2);
+    let arr = []
+    arr.push(1)
+    if (n === 1) {
         return arr
+    }
+    arr.push(2)
+    if (n === 2) {
+        return arr
+    }
+    arr.push("Fizz")
+    return arr
 }
