0df41b13e0b002ab65445174ebef84fe6822737f
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2018-06-19 17:53:49 +0100

    TEST - an argument of 15 gives [..., 11, "Fizz", 13, 14, "FizzBuzz - GREEN

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,8 +7,12 @@ export default function fizzbuzz(n) {
     let i = 0
 
     while (i < n) {
-        i += 1;
-        if (i % 3 === 0) {
+        i += 1
+
+        if (i % 3 === 0 && i % 5 == 0) {
+            arr.push("FizzBuzz");
+        } 
+        else if (i % 3 === 0) {
             arr.push("Fizz")
         } else if (i % 5 === 0) {
             arr.push("Buzz")
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,5 +1,10 @@
 import fizzbuzz from './fizzbuzz';
 
+// Example of bad naming - does not describe the spec
+// test this gives that is only good for understanding each step you are taking 
+// to develop, does not describe to the user the actual rules
+// maybe continue to refactor tests to give proper spec
+
 test('does nothing with no argument', () => {
   expect(fizzbuzz()).toBeUndefined();
 })
@@ -21,13 +26,13 @@ test(`an argument of 5 gives [1, 2, "Fizz", 4, "Buzz"]`, () => {
 })
 
 test(`an argument of 6 gives [1, 2, "Fizz", 4, "Buzz", "Fizz"]`, () => {
-  expect(fizzbuzz(6)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz"]);
+  expect(fizzbuzz(6)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz"])
 });
 
 test(`an argument of 10 gives [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"]`, () => {
-  expect(fizzbuzz(10)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"]);
+  expect(fizzbuzz(10)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"])
 });
 
 test(`an argument of 15 gives [..., 11, "Fizz", 13, 14, "FizzBuzz"]`, function () {
-  expect(fizzbuzz(15)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]);
+  expect(fizzbuzz(15)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"])
 });
