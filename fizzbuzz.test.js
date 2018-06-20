10e233174e36c3dd5fd6c82e58bf8696d3c7acbd
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2018-06-20 14:02:27 +0100

    FINAL REFACOR (refactored and added tests - used nesting to group the rules properly: 'Multiples of 3 say "Fizz"')

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -17,30 +17,40 @@ describe('A fizzbuzz game', () => {
       expect(fizzbuzz(2)).toEqual([1, 2])
     })
 
-    test(`an argument of 3 gives [1, 2, "Fizz"]`, () => {
-      expect(fizzbuzz(3)).toEqual([1, 2, "Fizz"])
+    describe('multiples of 3 say "Fizz"', () => {
+      test(`an argument of 3 gives [1, 2, "Fizz"]`, () => {
+        expect(fizzbuzz(3)).toEqual([1, 2, "Fizz"])
+      })
+      test(`an argument of 6 gives [1, 2, "Fizz", 4, "Buzz", "Fizz"]`, () => {
+        expect(fizzbuzz(6)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz"])
+      })
     })
 
-    test(`an argument of 5 gives [1, 2, "Fizz", 4, "Buzz"]`, () => {
-      expect(fizzbuzz(5)).toEqual([1, 2, "Fizz", 4, "Buzz"])
-    })
+    describe('multiples of 5 say "Buzz"', () => {
+      test(`an argument of 5 gives [1, 2, "Fizz", 4, "Buzz"]`, () => {
+        expect(fizzbuzz(5)).toEqual([1, 2, "Fizz", 4, "Buzz"])
+      })
+      test(`an argument of 10 gives [1, 2, "Fizz", 4, "Buzz", "Fizz", "Bang", 8, "Fizz", "Buzz"]`, () => {
+        expect(fizzbuzz(10))
+          .toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", "Bang", 8, "Fizz", "Buzz"])
+      })
+    })    
 
-    test(`an argument of 6 gives [1, 2, "Fizz", 4, "Buzz", "Fizz"]`, () => {
-      expect(fizzbuzz(6)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz"])
-    })
+    describe('multiples of 7 say "Bang"', () => {
+      test(`an argument of 7 gives [1, 2, "Fizz", 4, "Buzz", "Fizz", "Bang"]`, () => {
+        expect(fizzbuzz(7)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", "Bang"])
+      })
+      test(`an argument of 15 gives [..., 11, "Fizz", 13, 14, "FizzBuzz"]`, () => {
+        expect(fizzbuzz(14))
+          .toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", "Bang", 8, "Fizz", "Buzz", 11, "Fizz", 13, "Bang"])
+      })
+    })    
 
-    test(`an argument of 7 gives [1, 2, "Fizz", 4, "Buzz", "Fizz", "Bang"]`, () => {
-      expect(fizzbuzz(7)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", "Bang"])
-    })
-
-    test(`an argument of 10 gives [1, 2, "Fizz", 4, "Buzz", "Fizz", "Bang", 8, "Fizz", "Buzz"]`, () => {
-      expect(fizzbuzz(10))
-      .toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", "Bang", 8, "Fizz", "Buzz"])
-    })
-
-    test(`an argument of 15 gives [..., 11, "Fizz", 13, 14, "FizzBuzz"]`, () => {
-      expect(fizzbuzz(15))
-      .toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", "Bang", 8, "Fizz", "Buzz", 11, "Fizz", 13, "Bang", "FizzBuzz"])
+    describe('multiples of 3 and 5 say "FizzBuzz"', () => {
+      test(`an argument of 15 gives [..., 11, "Fizz", 13, 14, "FizzBuzz"]`, () => {
+        expect(fizzbuzz(15))
+          .toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", "Bang", 8, "Fizz", "Buzz", 11, "Fizz", 13, "Bang", "FizzBuzz"])
+      })   
     })
   })
 })
