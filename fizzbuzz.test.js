fdc2fb5f73be9f1f877781ff18426e7a73c5d361
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2018-06-20 11:33:54 +0100

    REFACTOR (nested tests using 'describe' for better readability)

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,33 +1,40 @@
 import fizzbuzz from './fizzbuzz';
 
-test('does nothing with no argument', () => {
-  expect(fizzbuzz()).toBeUndefined();
+
+describe('A fizzbuzz game', () => {
+
+  describe('takes a number as an argument and gives an array response', () => {
+
+    test('does nothing with no argument', () => {
+      expect(fizzbuzz()).toBeUndefined();
+    })
+
+    test('an argument of 1 gives [1]', () => {
+      expect(fizzbuzz(1)).toEqual([1])
+    })
+
+    test("an argument of 2 gives [1, 2]", () => {
+      expect(fizzbuzz(2)).toEqual([1, 2])
+    })
+
+    test(`an argument of 3 gives [1, 2, "Fizz"]`, () => {
+      expect(fizzbuzz(3)).toEqual([1, 2, "Fizz"])
+    })
+
+    test(`an argument of 5 gives [1, 2, "Fizz", 4, "Buzz"]`, () => {
+      expect(fizzbuzz(5)).toEqual([1, 2, "Fizz", 4, "Buzz"])
+    })
+
+    test(`an argument of 6 gives [1, 2, "Fizz", 4, "Buzz", "Fizz"]`, () => {
+      expect(fizzbuzz(6)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz"])
+    })
+
+    test(`an argument of 10 gives [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"]`, () => {
+      expect(fizzbuzz(10)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"])
+    })
+
+    test(`an argument of 15 gives [..., 11, "Fizz", 13, 14, "FizzBuzz"]`, () => {
+      expect(fizzbuzz(15)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"])
+    })
+  })
 })
-
-test('an argument of 1 gives [1]', () => {
-  expect(fizzbuzz(1)).toEqual([1]) 
-})
-
-test("an argument of 2 gives [1, 2]", () => {
-  expect(fizzbuzz(2)).toEqual([1, 2])
-})
-
-test(`an argument of 3 gives [1, 2, "Fizz"]`, () => {
-  expect(fizzbuzz(3)).toEqual([1, 2, "Fizz"])
-})
-
-test(`an argument of 5 gives [1, 2, "Fizz", 4, "Buzz"]`, () => {
-  expect(fizzbuzz(5)).toEqual([1, 2, "Fizz", 4, "Buzz"])
-})
-
-test(`an argument of 6 gives [1, 2, "Fizz", 4, "Buzz", "Fizz"]`, () => {
-  expect(fizzbuzz(6)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz"])
-});
-
-test(`an argument of 10 gives [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"]`, () => {
-  expect(fizzbuzz(10)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"])
-});
-
-test(`an argument of 15 gives [..., 11, "Fizz", 13, 14, "FizzBuzz"]`,  () => {
-  expect(fizzbuzz(15)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"])
-});
